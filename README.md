daf8d2eeb29785be4a559b156cf710174abd4dd5
sweeper3000 <liamcliff1@gmail.com>
2018-04-04 21:38:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# initserver
+A script that sets up a server to my personal liking
+
+# Tasks
+- Create new user, give it sudo access
+- Copy ```.ssh``` folder to new user's ```/home``` directory
+- Set ownership to the new user
+- Set permissions: 700 for ```.ssh```, 644 for ```.ssh/authorized_keys```
+- Check for and install server updates
+- Allow ports 22, 60000:60003 through ufw
+- Enable ufw
+- Install fail2ban and mosh
+
+This script will be updated as needs evolve.
---
+++ initserver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ask user what username they want
+echo "What username do you want for the new user?"
+read newuser
+
+# create a new user called main, give it sudo access
+adduser $newuser
+usermod -aG sudo main
+
+# copy the root's .ssh folder to the main's home directory
+cp -r /root/.ssh /home/main
+
+# cd to main's home folder
+cd /home/$newuser
+
+# change ownership of .ssh and contents to main
+chown main .ssh 
+chown main .ssh/authorized_keys
+
+# set permissions
+chmod 700 .ssh
+chmod 644 .ssh/authorized_keys
+
+# check for server updates and install them
+apt update && apt upgrade
+
+# install software
+apt install fail2ban mosh
+
+# add rules to ufw and enable it
+ufw allow 22
+ufw allow 60000:60003
+ufw enable
